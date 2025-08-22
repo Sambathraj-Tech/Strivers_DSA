de467c46a3eeae982d4001c56362ea87f6bcf30e
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-08-22 19:34:46 +0530

    Update paindrome_number.py

--- Basic_math/paindrome_number.py
+++ Basic_math/paindrome_number.py
@@ -1,2 +1,20 @@
-
+def reverse_number(n):
+    
+    check = n
+    result = 0
+    
+    while n > 0:
+        
+        last_digit = n % 10 # extract the lastdigit
+        result = (result * 10) + last_digit # add the extracted digit
+        n = n // 10 # remove the last digit for the next loop
+    
+    if check == result:
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+    
+n = int(input("Enter the number to be reversed: "))
+reverse_number(n)
+        
         
