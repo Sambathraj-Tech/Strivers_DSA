85befbf13c684ddd22389958c6651de2d64e67ed
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-08-31 18:31:01 +0530

    commit

--- Basic_math/palindrome_number.py
+++ Basic_math/palindrome_number.py
@@ -1,20 +1,12 @@
-def reverse_number(n):
-    
-    check = n
-    result = 0
-    
-    while n > 0:
-        
-        last_digit = n % 10 # extract the lastdigit
-        result = (result * 10) + last_digit # add the extracted digit
-        n = n // 10 # remove the last digit for the next loop
-    
-    if check == result:
-        print("Palindrome")
-    else:
-        print("Not Palindrome")
-    
-n = int(input("Enter the number to be reversed: "))
-reverse_number(n)
-        
-        
+	class Solution(object):
+	    def is_Palindrome(self, x):
+	        if x < 0:                         # x less than 0 or negative int return False
+	            return False
+	        elif x >= 0:                      # x greater than 0 or equal to 0 then proceed
+	            res = 0                       # Insert the reverse numbers in this variable
+	            copy = x                      # Take copy before reverse the number 
+	            while x > 0:                  
+	                temp = x % 10             # Take the last digit of the numbers
+	                res  = (res * 10) + temp  # 1st -> * 10 then add the temp (e.g: 5*10 = 50 (50+3 = 53))
+	                x    = x // 10            # Remove the last digit from the numbers
+            return res == copy                # Compare and return True
