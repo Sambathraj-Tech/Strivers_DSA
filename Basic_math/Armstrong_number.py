d9e631442f48036a19bb81ac3dbef4aedf081834
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-09-01 09:40:13 +0530

    commit

--- Basic_math/Armstrong_number.py
+++ Basic_math/Armstrong_number.py
@@ -1,4 +1,18 @@
 def armstrong_number(n):
+    res = 0
+    copy = n
+    while n > 0:
+        temp = n % 10
+        res = res + (temp ** 3)
+        n = n // 10
+    if res == copy:
+        print(f"{res} is a Armstrong Number.")
+    else:
+        print(f"{res} is not a Armstrong Number.")
+armstrong_number(153)
+=====================================================================================================================
+
+def armstrong_number(n):
     check = n
     num_digits = len(str(n)) # int > 1634 > str "1634" > len(4)
     sum = 0
